cef8ced4c36e7e089b6291b4e20f2fe3c69720fa
myst <mystster@gmail.com>
2023-12-12 23:31:59 +0900

    fix: can't build

--- keyboards/yoichiro/lunakey_pico/keymaps/mystster/keymap.c
+++ keyboards/yoichiro/lunakey_pico/keymaps/mystster/keymap.c
@@ -2,11 +2,29 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 // #include QMK_KEYBOARD_H
-#include ".build/obj_yoichiro_lunakey_pico/src/default_keyboard.h"
+// #include ".build/obj_yoichiro_lunakey_pico/src/default_keyboard.h"
+#include "quantum.h"
 #include "process_key_override.h"
 #include <quantum\keymap_extras\keymap_japanese.h>
 #include <quantum\rgblight\rgblight.h>
 #include <quantum\logging\debug.h>
+
+#define LAYOUT_split_3x6_4( \
+  L00, L01, L02, L03, L04, L05,           R00, R01, R02, R03, R04, R05, \
+  L10, L11, L12, L13, L14, L15,           R10, R11, R12, R13, R14, R15, \
+  L20, L21, L22, L23, L24, L25,           R20, R21, R22, R23, R24, R25, \
+                 L30, L31, L32, L33, R30, R31, R32, R33 \
+  ) \
+  { \
+    { L00, L01, L02, L03, L04, L05 }, \
+    { L10, L11, L12, L13, L14, L15 }, \
+    { L20, L21, L22, L23, L24, L25 }, \
+    { KC_NO, KC_NO, L30, L31, L32, L33 }, \
+    { R05, R04, R03, R02, R01, R00 }, \
+    { R15, R14, R13, R12, R11, R10 }, \
+    { R25, R24, R23, R22, R21, R20 }, \
+    { KC_NO, KC_NO, R33, R32, R31, R30 } \
+  }
 
 enum layer_number {
     _QWERTY = 0,
